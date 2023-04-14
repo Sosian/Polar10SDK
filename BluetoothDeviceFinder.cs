5875e878db4a77abbb28db996c67fcfb94f02c9f
Florian Decker <sosian@deckerhome.at>
2023-04-14 22:41:58 +0200

    initial commit

---
+++ BluetoothDeviceFinder.cs
@@ -0,0 +1,72 @@
+using Microsoft.Extensions.Logging;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+
+namespace PolarH10
+{
+    public class BluetoothDeviceFinder
+    {
+        private readonly ILogger<BluetoothDeviceFinder> _logger;
+
+        public BluetoothDeviceFinder (ILogger<BluetoothDeviceFinder> logger)
+        {
+            _logger = logger;
+        }
+
+        public DeviceInformation GetDevice()
+        {
+            var foundDevices = GetDevicesInternal();
+
+            _logger.LogDebug($"Found {foundDevices.Count} heart rate capable devices");
+            foreach (DeviceInformation device in foundDevices.ToList())
+            {
+                _logger.LogDebug($"{device.Name}: " +
+                    $"id = {device.Id}, " +
+                    $"default = {device.IsDefault}, " +
+                    $"enabled = {device.IsEnabled},  " +
+                    $"paired = {device.Pairing.IsPaired}");
+            }
+
+            return foundDevices[0];
+        }
+
+        private DeviceInformationCollection GetDevicesInternal()
+        {
+            var task = DeviceInformation.FindAllAsync(
+                GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.HeartRate),
+                new string[] { "System.Devices.ContainerId" });
+
+            DeviceInformationCollection? foundDevices = null;
+            try
+            {
+                while (true)
+                {
+                    _logger.LogDebug("Attempting to retrieve async result...");
+
+                    Thread.Sleep(100);
+
+                    var status = task.Status;
+
+                    if (status == Windows.Foundation.AsyncStatus.Canceled || task.Status == Windows.Foundation.AsyncStatus.Error)
+                        break;
+
+                    if (status == Windows.Foundation.AsyncStatus.Completed)
+                    {
+                        foundDevices = task.GetResults();
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                task.Close();
+            }
+
+            if (foundDevices == null)
+                throw new Exception("No Devices found");
+            
+            return foundDevices;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using PolarH10;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+var bluetoothDeviceFinder = new BluetoothDeviceFinder(host.Services.GetRequiredService<ILogger<BluetoothDeviceFinder>>());
+
+var polarH10Bluetooth = new PolarH10Bluetooth(host.Services.GetRequiredService<ILogger<PolarH10Bluetooth>>(), bluetoothDeviceFinder.GetDevice());
+polarH10Bluetooth.Start();
+
+await host.RunAsync();
